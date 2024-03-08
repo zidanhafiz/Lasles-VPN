5815f2426685bff99bffd1287a8cbb3dc51d01bf
zidanhafiz <nadizsonata@gmail.com>
2024-03-08 18:19:25 +0800

    remove unused font family

--- app/layout.tsx
+++ app/layout.tsx
@@ -1,9 +1,11 @@
 import type { Metadata } from 'next';
-import { Inter, Rubik } from 'next/font/google';
+import { Rubik } from 'next/font/google';
 import './globals.css';
 
-const inter = Inter({ subsets: ['latin'] });
-const rubix = Rubik({ weight: ['300', '400', '500', '600', '800'], subsets: ['latin'] });
+const rubix = Rubik({
+  weight: ['300', '400', '500', '600', '800'],
+  subsets: ['latin'],
+});
 
 export const metadata: Metadata = {
   title: 'Lasles VPN',
